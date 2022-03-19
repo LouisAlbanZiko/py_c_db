977c06de346dda1df0890485c4ce18ff725e46d3
peTheProgrammer <peTheProgrammer@gmail.com>
2022-03-19 17:33:38 +0100

    Added functions db_open and db_close

--- src/c_db_module.c
+++ src/c_db_module.c
@@ -1,35 +1,72 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-static PyObject *spam_system(PyObject *self, PyObject *args);
+#include <c_db/c_db.h>
+#include <c_core/c_core.h>
 
-static PyMethodDef SpamMethods[] = 
+static PyObject *py_cd_database_open(PyObject *self, PyObject *args);
+static PyObject *py_cd_database_close(PyObject *self, PyObject *args);
+
+static PyMethodDef py_c_db_methods[] =
 {
-	{"system", spam_system, METH_VARARGS, "Execute a shell command."}
+	{"db_open", py_cd_database_open, METH_VARARGS, "Python interface for cd_database_open C library function"},
+	{"db_close", py_cd_database_close, METH_VARARGS, "Python interface for cd_database_close C library function"},
+	{NULL, NULL, 0, NULL}
 };
 
-static PyModuleDef spammodule = 
+static struct PyModuleDef py_c_db_module =
 {
 	PyModuleDef_HEAD_INIT,
-	"spam", // name of module
-	NULL, // module documentation
-	-1, // size of per-interpreter state of the module, or -1 if the module keeps state in global variables.
-	SpamMethods
+	"py_c_db",
+	"Python interface for the c_db C library ",
+	-1,
+	py_c_db_methods
 };
 
-PyMODINIT_FUNC PyInit_spam()
-{
-	return PyModule_Create(&spammodule);
+PyMODINIT_FUNC PyInit_py_c_db(void) {
+	return PyModule_Create(&py_c_db_module);
 }
 
-static PyObject *spam_system(PyObject *self, PyObject *args)
+static PyObject *py_cd_database_open(PyObject *self, PyObject *args)
 {
-	const char *command;
-	int sts;
-	if(!PyArg_ParseTuple(args, "s", &command))
+	const char *_db_name;
+
+	/* Parse arguments */
+	if(!PyArg_ParseTuple(args, "s", &_db_name))
 	{
 		return NULL;
 	}
-	sts = system(command);
-	return PyLong_FromLong(sts);
+
+	CC_String db_name =
+	{
+		.data = _db_name,
+		.length = strlen(_db_name)
+	};
+
+	CD_Database *db = cd_database_open(db_name);
+	if(db == NULL)
+	{
+		CD_Error error = cd_get_last_error();
+		PyErr_SetString(PyExc_IOError, error.message.data);
+		return NULL;
+	}
+
+	return PyCapsule_New(db, NULL, NULL);
+}
+
+static PyObject *py_cd_database_close(PyObject *self, PyObject *args)
+{
+	PyObject *py_db;
+
+	/* Parse arguments */
+	if(!PyArg_ParseTuple(args, "O", &py_db))
+	{
+		return NULL;
+	}
+
+	CD_Database *db = (CD_Database *)PyCapsule_GetPointer(py_db, NULL);
+	cd_database_close(db);
+
+	Py_INCREF(Py_None);
+	return Py_None;
 }
